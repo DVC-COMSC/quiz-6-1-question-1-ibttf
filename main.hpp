56f95433835ec6291e513a97ed530bfeefd1e16d
roylee0912 <leeroy0912@gmail.com>
2024-03-15 22:12:55 -0700

    done i think

--- main.hpp
+++ main.hpp
@@ -1,8 +1,46 @@
+#include <iostream>
+using namespace std;
 
 //********************
 // Declare Function Prototypes
 //********************
+void getTwoValues(int& begin, int& end ){
+    cout << "Enter two values";
+    do {
+        cin>>begin>>end;
+    } while (begin < end);
+}
 
-// ******************************
-// Implement all your functions here
-// ******************************
+
+int getNextPrime(int begin){
+    int nextPrime = begin;
+    bool isPrime = false;
+    while (!isPrime){
+        nextPrime++;
+        isPrime = true;
+        for (int i = 2; i < nextPrime; i++){
+            if (nextPrime % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return nextPrime;
+    
+}
+
+int getPrevPrime(int end){
+    int lastPrime = end;
+    bool isPrime=false;
+    while (!isPrime){
+        lastPrime--;
+        isPrime = true;
+        for (int i = 2; i < lastPrime; i++){
+            if (lastPrime % i == 0){
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return lastPrime;
+}
